8af476af0a359c6c6e0c457986a85a2bd03c9755
hsy <che@gmail.com>
2014-10-08 11:09:47 +0800

    add fork test

---
+++ src/main.c
@@ -0,0 +1,18 @@
+/*
+ ============================================================================
+ Name        : test4c.c
+ Author      : abc
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	testFork();
+	return EXIT_SUCCESS;
+}
