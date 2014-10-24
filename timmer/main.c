e872c91d7df263931c752b7f3653ff6ca67efd27
hsy <che@gmail.com>
2014-10-24 13:56:52 +0800

    add timer

---
+++ timmer/main.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <time.h>
+#include <sys/time.h>
+#include <stdlib.h>
+#include <signal.h>
+
+static int count = 0;
+static struct itimerval oldtv;
+
+void set_timer() {
+	struct itimerval itv;
+	itv.it_interval.tv_sec = 1;
+	itv.it_interval.tv_usec = 0;
+	itv.it_value.tv_sec = 1;
+	itv.it_value.tv_usec = 0;
+	setitimer(ITIMER_REAL, &itv, &oldtv);
+}
+
+void signal_handler(int m) {
+	count++;
+	printf("%d\n", count);
+}
+
+int main() {
+	/*
+	 * 当setitimer()所执行的timer时间到了，会呼叫SIGALRM signal，
+	 * 所以行用signal()将要执行的function指定给SIGALRM。
+	 */
+	signal(SIGALRM, signal_handler);
+	set_timer();
+	while (count < 10000)
+		;
+	exit(0);
+	return 1;
+
+}
