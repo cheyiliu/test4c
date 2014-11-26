07e1a87f369abd4d4bf10b33e684204d22abfd3f
hsy <che@gmail.com>
2014-11-26 13:38:38 +0800

    add parm is array case

--- sizeof/main.c
+++ sizeof/main.c
@@ -23,24 +23,28 @@ struct struct_s3 { // 成员和struct_s2一样，顺序和类型，注意对比
 };
 #pragma pack ()
 
+void printSizeof(int arg[]) {
+	printf("sizeof(arg)   = %lu\n", sizeof(arg)); // 数组类型自动弱化为指针类型
+}
+
 int main() {
-	printf("\n---------szieof 基本类型------\n");
-	printf("szieof(char)   = %lu\n", sizeof(char));
-	printf("szieof(short)  = %lu\n", sizeof(short));
-	printf("szieof(int)    = %lu\n", sizeof(int));
-	printf("szieof(long)   = %lu\n", sizeof(long));
-	printf("szieof(float)  = %lu\n", sizeof(float));
-	printf("szieof(double) = %lu\n", sizeof(double));
+	printf("\n---------sizeof 基本类型------\n");
+	printf("sizeof(char)   = %lu\n", sizeof(char));
+	printf("sizeof(short)  = %lu\n", sizeof(short));
+	printf("sizeof(int)    = %lu\n", sizeof(int));
+	printf("sizeof(long)   = %lu\n", sizeof(long));
+	printf("sizeof(float)  = %lu\n", sizeof(float));
+	printf("sizeof(double) = %lu\n", sizeof(double));
 
-	printf("\n---------szieof 基本类型指针------\n");
-	printf("szieof(char*)   = %lu\n", sizeof(char*));
-	printf("szieof(short*)  = %lu\n", sizeof(short*));
-	printf("szieof(int*)    = %lu\n", sizeof(int*));
-	printf("szieof(long*)   = %lu\n", sizeof(long*));
-	printf("szieof(float*)  = %lu\n", sizeof(float*));
-	printf("szieof(double*) = %lu\n", sizeof(double*));
+	printf("\n---------sizeof 基本类型指针------\n");
+	printf("sizeof(char*)   = %lu\n", sizeof(char*));
+	printf("sizeof(short*)  = %lu\n", sizeof(short*));
+	printf("sizeof(int*)    = %lu\n", sizeof(int*));
+	printf("sizeof(long*)   = %lu\n", sizeof(long*));
+	printf("sizeof(float*)  = %lu\n", sizeof(float*));
+	printf("sizeof(double*) = %lu\n", sizeof(double*));
 
-	printf("\n---------szieof 结构体，涉及结构体对齐的知识点------\n");
+	printf("\n---------sizeof 结构体，涉及结构体对齐的知识点------\n");
 	printf("sizeof(struct s1) = %lu\n", sizeof(struct struct_s1));
 	printf("sizeof(struct s2) = %lu\n", sizeof(struct struct_s2));
 	printf("sizeof(struct s3) = %lu\n", sizeof(struct struct_s3));
@@ -53,13 +57,17 @@ int main() {
 	printf("address of ss2.c = %p\n", &ss2.c);
 	printf("address of ss2.d = %p\n", &ss2.d);
 
-	printf("\n---------szieof 稍微复杂点的例子------\n");
+	printf("\n---------sizeof 稍微复杂点的例子------\n");
 	double* (*a)[3][6]; // a is a pointer to array[3][6], the array is of type double*;
 	printf("%lu\n", sizeof(a)); // a为指针
 	printf("%lu\n", sizeof(*a)); // *a为一个有3*6个指针元素的数组
 	printf("%lu\n", sizeof(**a)); // **a为数组一维的6个指针
 	printf("%lu\n", sizeof(***a)); // ***a为一维的第一个指针
 	printf("%lu\n", sizeof(****a)); // ****a为一个double变量
+
+	printf("\n---------sizeof 函数参数------\n");
+	int array[10] = { 0, };
+	printSizeof(array);
 
 	return 0;
 }
