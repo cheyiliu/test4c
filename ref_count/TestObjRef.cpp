2e4672b4890d2c289cdd7e1bc43ef1ef46df31bd
hsy <che@gmail.com>
2014-12-17 15:20:02 +0800

    增加smart_ptr

--- ref_count/TestObjRef.cpp
+++ ref_count/TestObjRef.cpp
@@ -17,15 +17,46 @@ public:
 	}
 };
 
+class UseTestObj {
+
+public:
+	TestObj* mObj;
+	void setTestObj(TestObj* obj) {
+		mObj = obj;
+		mObj->retain();
+	}
+	~UseTestObj() {
+		printf("~UseTestObj\n");
+		mObj->release();
+	}
+};
+/*
+ * release retain配对 依然麻烦， 我们需要只能指针， 见../smart_ptr
+ */
 int main() {
-	TestObj * ptr = new TestObj(); ptr->print();//初始化时引用计数为1
-	ptr->retain();	ptr->print();
-	ptr->retain();	ptr->print();
-	ptr->retain();	ptr->print();
+	TestObj * ptr = new TestObj();
+	ptr->print(); //初始化时引用计数为1
+	ptr->retain();
+	ptr->print();
+	ptr->retain();
+	ptr->print();
+	ptr->retain();
+	ptr->print();
 
 	ptr->release();
 	ptr->release();
 	ptr->release();
+
+	{
+		TestObj testobj;
+	}
+
+	{
+		UseTestObj useTestObj;
+		useTestObj.setTestObj(ptr);
+	}
+
 	ptr->release();
+	printf("end of main\n");
 }
 #endif // __TEST_OBJ_H__
---
+++ smart_ptr/shared_ptr.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <boost/shared_ptr.hpp>
+using namespace std;
+using namespace boost;
+class Test {
+public:
+	Test(string s) {
+		str = s;
+		cout << " Test:" << str << endl;
+	}
+	~Test() {
+		cout << "~Test:" << str << endl;
+	}
+	string& getStr() {
+		return str;
+	}
+	void setStr(string s) {
+		str = s;
+	}
+	void print() {
+		cout << str << endl;
+	}
+private:
+	string str;
+};
+
+/*
+ * http://www.cnblogs.com/hujian/archive/2012/12/10/2810754.html
+ * https://github.com/cheyiliu/All-in-One/wiki/smart_ptr
+ * */
+int main() {
+	{
+		shared_ptr<Test> ptest123(new Test("ptest123"));
+		shared_ptr<Test> ptest456(new Test("ptest456"));
+
+		cout << ptest123.use_count() << endl;
+		cout << ptest456.use_count() << endl;
+
+		ptest456 = ptest123;
+
+		cout << ptest123.use_count() << endl;
+		cout << ptest456.use_count() << endl;
+	}
+	{
+		Test *pTest = new Test(
+				"Test share the same obj by more than one shared_ptr");
+		shared_ptr<Test> temp1(pTest);
+		assert(temp1.use_count() == 1);
+//		shared_ptr<Test> temp2(pTest);
+//		assert(temp2.use_count() == 1);
+	} // temp1和temp2都离开作用域，它们都销毁pInt，会导致两次释放同一块内存
+	cout << "end of main !\n";
+	return 0;
+}
